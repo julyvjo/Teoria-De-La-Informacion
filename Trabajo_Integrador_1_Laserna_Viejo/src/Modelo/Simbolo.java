814a738f4fc135d5b51279c4525512606b284fba
july <july@DESKTOP-Q73IOOQ>
2020-09-26 17:03:42 -0300

    prueba 2

--- Trabajo_Integrador_1_Laserna_Viejo/src/Modelo/Simbolo.java
+++ Trabajo_Integrador_1_Laserna_Viejo/src/Modelo/Simbolo.java
@@ -3,41 +3,48 @@ package Modelo;
 public class Simbolo {
 	private char valor;
 	private float probabilidad;
-	
+
 	public Simbolo(char valor, float probabilidad) {
-		
+
 		this.valor = valor;
 		this.probabilidad = probabilidad;
 	}
-	
+
 	public char getValor() {
 		return valor;
 	}
+
 	public void setValor(char valor) {
 		this.valor = valor;
 	}
+
 	public float getProbabilidad() {
 		return probabilidad;
 	}
+
 	public void setProbabilidad(float probabilidad) {
 		this.probabilidad = probabilidad;
 	}
-	
-	
-	
+
+	/*
+	 * 
+	 */
 	public float getInformacionBin() {
-		return (float) (Math.log10(1/probabilidad) / Math.log10(2));
-	}
-	
-	public float getInformacionNat() {
-		return (float) Math.log(1/probabilidad);
-	}
-	
-	public float getInformacionHart() {
-		return (float) Math.log10(1/probabilidad);
+		return (float) (Math.log10(1 / probabilidad) / Math.log10(2));
 	}
 
-	
-	
-	
+	/*
+	 * 
+	 */
+	public float getInformacionNat() {
+		return (float) Math.log(1 / probabilidad);
+	}
+
+	/*
+	 * 
+	 */
+	public float getInformacionHart() {
+		return (float) Math.log10(1 / probabilidad);
+	}
+
 }
