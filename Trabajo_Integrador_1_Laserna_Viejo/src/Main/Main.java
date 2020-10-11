47cfddc41cbb19dd0a5aede507cb7546ccaf4692
lautalaserna <lasernalauta@gmail.com>
2020-10-11 17:47:52 -0300

    Memoria Nula

--- Trabajo_Integrador_1_Laserna_Viejo/src/Main/Main.java
+++ Trabajo_Integrador_1_Laserna_Viejo/src/Main/Main.java
@@ -1,11 +1,18 @@
 package Main;
 
+import Modelo.FuenteMemoriaNula;
+import Modelo.SimboloMemoriaNula;
+
 public class Main {
 
 	public static void main(String[] args) {
 
-		// comentario de july 4
-
+		FuenteMemoriaNula f = new FuenteMemoriaNula();
+		f.agregarSimbolo(new SimboloMemoriaNula('A',0.25));
+		f.agregarSimbolo(new SimboloMemoriaNula('B',0.25));
+		f.agregarSimbolo(new SimboloMemoriaNula('C',0.25));
+		f.agregarSimbolo(new SimboloMemoriaNula('D',0.25));
+		System.out.println(f.getEntropia());
 	}
 
 }
