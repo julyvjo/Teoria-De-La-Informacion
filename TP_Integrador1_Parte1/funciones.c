ef97bd43617de5490f2765a29978a4f59eb670df
julyvjo <julyvjo@gmail.com>
2020-10-21 19:38:29 -0300

    parte 1 lista

--- TP_Integrador1_Parte1/funciones.c
+++ TP_Integrador1_Parte1/funciones.c
@@ -5,6 +5,7 @@
 #include "funciones.h"
 
 #define N_MAX 10
+#define ITERACIONES 7
 
 
 //*****************************************************
@@ -15,7 +16,7 @@ void MemNula(){
     double prob[N_MAX];
     double vecCantInfo[N_MAX];
     int ext[10] = {0,1,2,3,4,5,6,7,8,9};
-    int n=0,k=1;
+    int n=0,k=0;
 
     leerMemNula(prob,&n);
 
@@ -24,14 +25,18 @@ void MemNula(){
 
 
     //mostramos resultados sin extension
-    mostrarResultados(prob,vecCantInfo,n);
+    mostrarResultadosNula(prob,vecCantInfo,n);
 
 
     //extension ?
+    printf("Desea calcular extension? ingrese K o 0 para salir\n\n");
 
-    //mostrar resultados
+    scanf("%d",&k);
 
-
+    if( 0 < k && k < 11){
+        extension(ext,prob,n,0,k);
+        printf("\nH(S^%d) = %lf\n",k,entropiaNula(prob,vecCantInfo,n)*k);
+    }
 
 }
 
@@ -102,7 +107,7 @@ void extension(int ext[], double prob[], int n, int h, int k){
 
         //printf("simbolo ");
         for(int i=0; i<k; i++){
-            printf("%d",ext[i]);
+            printf("S%d",ext[i]);
         }
         printf("\t%lf",probabilidad);
         printf("\t%lf \n",cantInfo);
@@ -115,7 +120,7 @@ void extension(int ext[], double prob[], int n, int h, int k){
     }
 }
 
-void mostrarResultados(double prob[], double vecCantInfo[], int n){
+void mostrarResultadosNula(double prob[], double vecCantInfo[], int n){
 
     printf("**************** RESULTADOS ****************\n\n");
 
@@ -129,16 +134,148 @@ void mostrarResultados(double prob[], double vecCantInfo[], int n){
     printf("********************************************\n\n");
 }
 
-//*****************************************************
+//********************************************************************************************************************************************************************************************************************
 //funciones para markov
 
 void Markov(){
 
+    double mat[N_MAX][N_MAX];
+    double v[N_MAX];
+    int n;
+
+    //leemos la matriz
+    leerMarkov(mat,&n);
+
+    //calculamos V*
+    calcula_V(mat,v,n);
+
+    //mostrar resultados
+    mostrarResultadosMarkov(mat,v,n);
+
+
 }
 
-void leerMarkov(){
+/*
+FORMATO DE LECTURA
+
+n
+matriz
+
+*/
+void leerMarkov(double mat[][N_MAX], int *n){
+
+    FILE *arch;
+
+    arch = fopen("markov.txt","rt");
+
+    if(arch == NULL){
+        printf("Archivo markov.txt no encontrado");
+        exit(1);
+    }else{
+        fscanf(arch,"%d",n);
+
+        for(int i=0; i<*n; i++){
+            for(int j=0; j<*n; j++){
+                fscanf(arch,"%lf",&(mat[i][j]));
+            }
+        }
+    }
+
+    fclose(arch);
 
 }
+
+void calcula_V(double mat[][N_MAX], double v[], int n){
+
+    double mAux[N_MAX][N_MAX];
+
+    //copiamos la matriz original para no perderla
+    copia_matriz(mAux,mat,n);
+
+    //itera hasta llegar al estado estacionario de la matriz
+    for(int i=0; i<ITERACIONES; i++ ){
+        mat_cuadrada(mAux,n);
+    }
+
+    for(int i=0; i<n; i++)
+        v[i] = mAux[i][1];
+
+}
+
+void mat_cuadrada(double mat[][N_MAX],int n){
+
+        double acum;
+        double mAux[N_MAX][N_MAX];
+
+        copia_matriz(mAux,mat,n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                acum = 0;
+                for (int k = 0; k < n; k++) {
+                    acum += mAux[i][k] * mAux[k][j];
+                }
+                mat[i][j] = acum;
+            }
+        }
+
+}
+
+void copia_matriz(double mat2[][N_MAX], double mat[][N_MAX], int n){
+
+    for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                mat2[i][j] = mat[i][j];
+            }
+    }
+
+}
+
+double entropiaMarkov(double mat[][N_MAX], double v[N_MAX], int n){
+
+    double entropia = 0, auxColumna = 0; //auxColumna es la sumatoria de la columna
+
+    for(int j=0; j<n; j++){
+            auxColumna = 0;
+            for(int i=0; i<n; i++){
+                auxColumna += mat[i][j] * info(mat[i][j]);
+            }
+        entropia += v[j] * auxColumna;
+    }
+
+    return entropia;
+}
+
+void mostrarResultadosMarkov(double mat[][N_MAX], double v[N_MAX], int n){
+
+    printf("**************** RESULTADOS ****************\n\n");
+
+    printf("Matriz de transicion:\n\n");
+
+    //mostrar matriz
+    for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                printf("%6.3lf ",mat[i][j]);
+            }
+            printf("\n");
+    }
+
+    //mostrar V*
+    printf("\nV*: {");
+    for(int i=0; i<n; i++){
+        printf("%4.3lf",v[i]);
+        if( i != n-1 )
+            printf(", ");
+    }
+    printf("}\n");
+
+
+    printf("\nH(s) = %7.4f\n\n",entropiaMarkov(mat,v,n));
+
+    printf("********************************************\n\n");
+}
+
+
 
 //*****************************************************
 //funciones comunes
--- TP_Integrador1_Parte1/funciones.h
+++ TP_Integrador1_Parte1/funciones.h
@@ -1,3 +1,4 @@
+#define N_MAX 10
 //*****************************************************
 //funciones para memoria nula
 
@@ -11,14 +12,24 @@ double entropiaNula(double prob[], double vecCantInfo[], int n);
 
 void extension(int ext[], double prob[], int n, int h, int k);
 
-void mostrarResultados(double prob[], double vecCantInfo[], int n);
+void mostrarResultadosNula(double prob[], double vecCantInfo[], int n);
 
 //*****************************************************
 //funciones para markov
 
 void Markov();
 
-void leerMarkov();
+void leerMarkov(double mat[][N_MAX], int *n);
+
+void calcula_V(double mat[][N_MAX], double v[], int n);
+
+void mat_cuadrada(double mat[][N_MAX],int n);
+
+void copia_matriz(double mat2[][N_MAX], double mat[][N_MAX], int n);
+
+double entropiaMarkov(double mat[][N_MAX], double v[N_MAX], int n);
+
+void mostrarResultadosMarkov(double mat[][N_MAX], double v[N_MAX], int n);
 
 //*****************************************************
 //funciones comunes
