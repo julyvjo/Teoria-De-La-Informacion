1246575ee0280ac4fa0e8c3d31376bcb49cac772
julyvjo <julyvjo@gmail.com>
2020-11-17 17:24:43 -0300

    parte 2 inicio

---
+++ TP_Integrador2/src/modelo/main.java
@@ -0,0 +1,30 @@
+package modelo;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		//leer un txt y a partir de eso obtener los simbolos con sus probabilidades
+		
+		//una vez obtenida la estructura simbolo-probabilidad codificar para huffman, shanon-fano y RLC (para ambas fuentes)
+		
+		//mostrar resultados
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
